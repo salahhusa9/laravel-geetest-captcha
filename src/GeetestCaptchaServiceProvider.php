bf694b056c3d117295f2e39d85940a459a915432
salahhusa9 <salahhusa9@gmail.com>
2024-07-04 12:20:03 +0100

    Refactor GeetestCaptchaServiceProvider to use new Geetest API

--- src/GeetestCaptchaServiceProvider.php
+++ src/GeetestCaptchaServiceProvider.php
@@ -19,7 +19,7 @@ class GeetestCaptchaServiceProvider extends PackageServiceProvider
          */
         $package
             ->name('laravel-geetest-captcha')
-            ->hasConfigFile()
+            // ->hasConfigFile()
             ->hasViews()
             ->hasCommand(GeetestCaptchaCommand::class);
 
@@ -28,9 +28,28 @@ class GeetestCaptchaServiceProvider extends PackageServiceProvider
         });
 
         Blade::directive('geetestCaptchaInit', function ($elementId) {
-            return View::make('geetest-captcha::initGeetest4', [
-                'elementId' => $elementId,
-            ])->render();
+
+            $html = <<<HTML
+                <!-- GEETEST -->
+                <script>
+                    var captchaId = "{{ env('GEETEST_ID') }}"
+
+                    initGeetest4({
+                        captchaId: captchaId,
+                    }, function (geetest) {
+                        window.geetest = geetest
+                        geetest
+                            .appendTo("#{{ $elementId }}")
+                            .onSuccess(function (e) {
+                                let result = geetest.getValidate();
+                                $("#{{ $elementId }}").parent().append('<input type="hidden" name="geetest_captcha_data" value="' + result + '">');
+                            })
+                    });
+
+                </script>
+            HTML;
+
+            return $html;
         });
     }
 }
