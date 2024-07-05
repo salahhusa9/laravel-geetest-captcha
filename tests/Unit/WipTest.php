f4b61133ca606bebb3494c86c721771bd540766c
salahhusa9 <salahhusa9@gmail.com>
2024-07-05 18:45:25 +0100

    Fix bug in login functionality

---
+++ tests/Unit/WipTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Salahhusa9\GeetestCaptcha\Tests\Unit;
+
+use Salahhusa9\GeetestCaptcha\Tests\TestCase;
+
+class WipTest extends TestCase
+{
+    /** @test */
+    public function it_can_run_tests()
+    {
+        $this->assertTrue(true);
+    }
+}
+
